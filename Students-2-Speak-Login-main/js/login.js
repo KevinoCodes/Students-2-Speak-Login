54af170785e742c228059978335909aef5e549b4
KevinoCodes <58299737+KevinoCodes@users.noreply.github.com>
2021-06-07 20:21:31 -0400

    Update login.js

--- Students-2-Speak-Login-main/js/login.js
+++ Students-2-Speak-Login-main/js/login.js
@@ -4,9 +4,7 @@ function validate() {
   if (classcode === "21SDP") {
     console.log("success");
 
-    window.location.replace(
-      "https://sites.google.com/view/students-2-speak/home?authuser=0"
-    );
+    window.location.href = "classLink.html";
   } else {
     console.log("error");
     window.alert("Incorrect Class Code, please check case sensitivity");
